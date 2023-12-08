477338f30668fded8ef144089daa8cc5fb2a3465
amir.ebrahimi <amirkingofnight@gmail.com>
2023-12-08 16:39:04 +0330

    first commit

---
+++ src/data/home.ts
@@ -0,0 +1,154 @@
+export const categories = [
+  "All",
+  "JavaScript",
+  "TypeScript",
+  "Programming",
+  "Game",
+  "Music",
+  "Educational",
+  "Next.js",
+  "React",
+  "Gta VI",
+  "Front-End Web Development",
+  "Back-End Web Development",
+  "Night",
+];
+
+export const videos = [
+  {
+    id: "1",
+    title: "Dark Side Of The Money That You Don't Know ",
+    channel: {
+      name: "Money Crazy",
+      id: "WebDevSimplified",
+      profileUrl:
+        "https://i.pinimg.com/564x/4f/37/0a/4f370a2213ba4eaf08859b19416ab85e.jpg",
+    },
+    views: 222536,
+    postedAt: new Date("2023-08-29"),
+    duration: 938,
+    thumbnailUrl:
+      "https://i.pinimg.com/564x/f5/54/10/f5541073b7876505eb7dc85425ba67b2.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "2",
+    title: "Chatgbt Vs Google",
+    channel: {
+      name: "Freak",
+      id: "WebDevSimplified",
+      profileUrl:
+        "https://i.pinimg.com/564x/3d/7a/af/3d7aaf6e390072d10b83bbee4c89637d.jpg",
+    },
+    views: 257136,
+    postedAt: new Date("2023-08-22"),
+    duration: 732,
+    thumbnailUrl:
+      "https://i.pinimg.com/564x/4b/92/de/4b92de6bb90c852bceb7c53195b1538b.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "3",
+    title: "The Secret OF Tesla",
+    channel: {
+      name: "Elon Musk",
+      id: "KevinPowell",
+      profileUrl:
+        "https://i.pinimg.com/564x/76/4e/36/764e361b24566bb1b27d051662c0271c.jpg",
+    },
+    views: 1232300,
+    postedAt: new Date("2023-10-05"),
+    duration: 120,
+    thumbnailUrl:
+      "https://i.pinimg.com/564x/3f/c0/ba/3fc0baf3a6ef36d7532462837619d490.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "4",
+    title: "Dark Truth About Andrew Tate",
+    channel: {
+      name: "Andrew Tate",
+      id: "KevinPowell",
+      profileUrl:
+        "https://i.pinimg.com/564x/fb/0e/14/fb0e146fc21bd7934d287adccfe15b2d.jpg",
+    },
+    views: 112,
+    postedAt: new Date("2023-9-19"),
+    duration: 4343,
+    thumbnailUrl:
+      "https://i.pinimg.com/564x/0e/c0/bb/0ec0bbf7a8ca0bf9e66cc07623053d7d.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "5",
+    title: "Mike Tyson or One Punch Man",
+    channel: {
+      name: "Mike Tyson",
+      id: "WebDevSimplified",
+      profileUrl:
+        "https://i.pinimg.com/564x/fc/43/4b/fc434b0b01f068953e5c6f4667707317.jpg",
+    },
+    views: 42345,
+    postedAt: new Date("2023-03-02"),
+    duration: 1000,
+    thumbnailUrl:
+      "https://i.pinimg.com/564x/02/97/8e/02978ed45726ea1e1404be1ac0cdc488.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "6",
+    title: "I Got Laid Off...",
+    channel: {
+      name: "Armagedon",
+      id: "CalebCurry",
+      profileUrl:
+        "https://i.pinimg.com/564x/ac/1b/a4/ac1ba41e72ebb41f8c519d0e5d9f4d2d.jpg",
+    },
+    views: 10340,
+    postedAt: new Date("2023-03-03"),
+    duration: 54,
+    thumbnailUrl:
+      "https://i.pinimg.com/564x/cd/a4/f3/cda4f315cf9629ddd77a65e6469c1b6e.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "7",
+    title: "Say Hello to Robots",
+    channel: {
+      name: "Antony",
+      id: "Fireship",
+      profileUrl:
+        "https://i.pinimg.com/564x/9a/53/d5/9a53d5244451b063458d666536645c49.jpg",
+    },
+    views: 10323340,
+    postedAt: new Date("2023-08-09"),
+    duration: 100,
+    thumbnailUrl:
+      "https://i.pinimg.com/564x/af/9e/fb/af9efbfde3b0e74b16bbb6092c600a0e.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "8",
+    title: "Game Award Of the Year",
+    channel: {
+      name: "king",
+      id: "Fireship",
+      profileUrl:
+        "https://i.pinimg.com/564x/72/31/21/723121ec508800e7590d811784925b21.jpg",
+    },
+    views: 20323340,
+    postedAt: new Date("2023-09-09"),
+    duration: 105,
+    thumbnailUrl:
+      "https://i.pinimg.com/564x/1e/68/67/1e68670a817dc9a922d63ba55524d85a.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+];
---
+++ src/data/sidebar.ts
@@ -0,0 +1,46 @@
+export const subscriptions = [
+  {
+    channelName: "Fireship",
+    id: "Fireship",
+    imgUrl:
+      "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "King",
+    id: "CalebCurry",
+    imgUrl:
+      "https://i.pinimg.com/564x/72/31/21/723121ec508800e7590d811784925b21.jpg",
+  },
+  {
+    channelName: "Elon Musk",
+    id: "ElonMusk",
+    imgUrl:
+      "https://i.pinimg.com/564x/76/4e/36/764e361b24566bb1b27d051662c0271c.jpg",
+  },
+  {
+    channelName: "Andrew Tate",
+    id: "AndrewTate",
+    imgUrl:
+      "https://i.pinimg.com/564x/fb/0e/14/fb0e146fc21bd7934d287adccfe15b2d.jpg",
+  },
+  {
+    channelName: "Sonny Sangha",
+    id: "SonnySangha",
+    imgUrl:
+      "https://yt3.ggpht.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s68-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "Traversy Media",
+    id: "TraversyMedia",
+    imgUrl:
+      "https://yt3.googleusercontent.com/ytc/APkrFKYcYswt_UhD7D0j6ddiQz6Gb8Q_vSJOjhYI0CoXSw=s176-c-k-c0x00ffffff-no-rj-mo",
+  },
+];
+
+export const playlists = [
+  { id: "1", name: "Frontend & Backend" },
+  { id: "2", name: "Favorites" },
+  { id: "3", name: "React" },
+  { id: "4", name: "Non-Dev" },
+  { id: "5", name: "TypeScript" },
+];
